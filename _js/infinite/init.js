ffc38288f2730ae3a055c76f594452e8989510ee
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-05-15 09:10:11 -0700

    fix inifinite scroll, add dark mode init

--- _js/infinite/init.js
+++ _js/infinite/init.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
-    $('.articles').jscroll({
-        contentSelector: 'article',
-        nextSelector: '.next',
-        callback: removeWrap,
-    });    
+	$('.articles').jscroll({
+		contentSelector: 'article, .pagination',
+		nextSelector: '.next',
+		callback: removeWrap,
+	});
 });
 
-function removeWrap(){
-    $('.jscroll-added article').unwrap();
+function removeWrap() {
+	var thisTheme = document.documentElement.style.getPropertyValue("color-scheme");
+	const thisElements = $('.jscroll-added article');
+	var forceDarkMode = (thisTheme == 'dark');
+	for (i = 0; i < thisElements.length; i++) {
+		thisElements[i].classList.toggle("dark-theme", forceDarkMode);
+	}
+
+	$('.jscroll-added article').unwrap();
 }
