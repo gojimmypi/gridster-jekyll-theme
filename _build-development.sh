eac81d9e88f380417cb8e1ca3ca5f8d80449b552
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-05-15 09:43:47 -0700

    added install checks; --assume-yes no longer launches new browser

--- _build-development.sh
+++ _build-development.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
+echo "$1"
 
-echo "value=$1"
+
+if command -v bundle &> /dev/null ; then
+  echo "Confirmed bundle is installed"
+else
+  echo "ERROR: It appears bundle is not installed."
+  exit 1
+fi
+
+bundle exec jekyll --help &> /dev/null
+if [ $? -eq 0 ]; then
+  echo "Confirmed jekyll bundle is installed"
+else
+  echo 'Error: bundle exec jekyll is not installed or returned an error.' >&2
+  exit 1
+fi
+
+if command -v sass &> /dev/null ; then
+  echo "Confirmed sass is installed"
+else
+  echo ""
+  echo "WARNING: It appears sass is not installed. See https://sass-lang.com/install"
+  echo ""
+fi
+
+if command -v grunt &> /dev/null ; then
+  echo "Confirmed appears is installed"
+else
+  echo ""
+  echo "WARNING: It appears grunt is not installed. File transformations in gruntfile.js will NOT occur."
+  echo ""
+fi
+
+bundle check
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -30,7 +63,13 @@ else
   read -p "Delete files in $SITE_DIR? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] &&  if [ -d "$SITE_DIR" ]; then rm -Rf $SITE_DIR; fi
 fi
 
-echo "launching browser for baseurl=$baseurl found in_config.yml"
-python3 -mwebbrowser http://127.0.0.1:4000/$baseurl/
+if [ "$1" == "--assume-yes" ]; then
+  echo "Skipping browser launch for $1"
+else
+  echo "launching browser for baseurl=$baseurl found in_config.yml"
+  python3 -mwebbrowser http://127.0.0.1:4000/$baseurl/
+fi
+
+echo "bundle exec jekyll serve --profile --incremental --drafts"
 bundle exec jekyll serve --profile --incremental --drafts
 
